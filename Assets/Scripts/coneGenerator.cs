207219a72b6698ff364992615a8df62a3a4e6501
Gezzellig <knaap@live.com>
2018-01-26 21:32:50 +0100

    Basics of cones stuff

---
+++ Assets/Scripts/coneGenerator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coneGenerator : MonoBehaviour {
+    public GameObject cone;
+    public Material mat;
+	// Use this for initialization
+	void Start () {
+        GameObject locCone = Instantiate(cone);
+        locCone.GetComponent<Renderer>().material = mat;
+        locCone.transform.position = transform.position;
+        locCone.transform.parent = transform;
+	}
+
+}
