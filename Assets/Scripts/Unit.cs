5edff6b4af86e9d4e33d976bc48d0f09fd34ab9b
Timo Strating <timostrating@home.nl>
2018-01-26 21:02:36 +0100

    random name generated branch with game scene with player

---
+++ Assets/Scripts/Unit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour {
+
+	Transform myTransform;
+	Rigidbody2D rigidbody;
+	[SerializeField]
+	float speed = 10;
+
+	// Use this for initialization
+	void Start () {
+		myTransform = gameObject.transform;
+		rigidbody = gameObject.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+		// myTransform.Translate(movement.normalized * speed * Time.deltaTime );
+		rigidbody.AddForce(movement * speed);
+	}
+}
