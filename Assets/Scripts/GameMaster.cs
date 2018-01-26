f32266e4a817f766cbf4e54672b38958dd7108d3
Timo Strating <timostrating@home.nl>
2018-01-26 22:57:21 +0100

    aanpassingen

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+	Animation animation;
+	GameObject playerPrefab;
+
+	List<Unit> units = new List<Unit>();
+
+
+	private void Start() {
+		Vector3 randomPos = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
+		GameObject unitObject = Instantiate(playerPrefab, randomPos, Quaternion.identity) as GameObject;
+		units.Add(unitObject.GetComponent<Unit>());		
+	}
+
+	private void Update() {
+
+	}
+}
---
+++ Assets/Scripts/Gun.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour {
+
+	[SerializeField] Transform shootPosition;
+	[SerializeField] GameObject bullet;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			Instantiate(bullet, shootPosition.position, Quaternion.identity);
+		}
+	}
+}
