69f29e713778728f7732120fc66dc265e95fce5e
tincoboekestijn <tinco.boekestijn@gmail.com>
2018-01-27 19:41:59 +0100

    Add debris

--- Assets/Scripts/DebrisController.cs
+++ Assets/Scripts/DebrisController.cs
@@ -4,31 +4,52 @@ using UnityEngine;
 
 public class DebrisController : MonoBehaviour {
 
-	public GameObject[] cells;
-
+//	public GameObject[] cells;
 	// Use this for initialization
+
+	public float DebreeInterval = 10.0f;
+	public float tChange = 3.0f;
+	public GameObject debris;
+
 	void Start () {
 		
 	}
-	
-	// Update is called once per frame
+
 	void Update () {
-		
+		tChange -= Time.deltaTime;
+
+		if (tChange <= 0) {
+			float randomX = Random.Range (-8.0f, 8.0f);
+			float randomY = Random.Range (-4.5f, 4.5f);
+			GameObject currDebris = Instantiate (debris, new Vector3(randomX, randomY, 10.0f), new Quaternion(0.0f, 0.0f, 0.0f, 0.0f)) as GameObject;
+			currDebris.GetComponent<DebrisSimulator> ().dc = this;
+			tChange = Random.Range (DebreeInterval, DebreeInterval + 5.0f);
+		}
 	}
 
-//	GameObject findClosestCell () 
-//	{
-//		cells = GameObject.FindGameObjectsWithTag("Cells");
-//		GameObject selectedCell;
-//		float closestDistance = float.MaxValue;
-//		foreach(GameObject cell in cells ) {
-//			var pos = cell.transform.position;
-//			float distance = Mathf.Sqrt (Mathf.Pow(pos.x - transform.position.x, 2) + Mathf.Pow(pos.y - transform.position.y, 2));
-//			if (closestDistance > distance) {
-//				closestDistance = distance;
-//				selectedCell = cell;
-//			}
-//		}
-//		return closestDistance;
-//	}
+	public void HitPlayer(float x, float y)
+	{
+		GameObject hittedPlayer = this.findClosestPlayer (x, y);
+
+		if(hittedPlayer != null) {
+			hittedPlayer.GetComponent<Unit> ().Infect ();
+		}
+
+	}
+
+	public GameObject findClosestPlayer (float x, float y) 
+	{
+		GameObject[] players = GameObject.FindGameObjectsWithTag("Player");
+		float closestDistance = float.MaxValue;
+		GameObject selectedPlayer = null;
+		foreach(GameObject player in players ) {
+			Vector3 pos = player.transform.position;
+			float distance = Mathf.Sqrt (Mathf.Pow(pos.x - x, 2) + Mathf.Pow(pos.y - y, 2));
+			if (closestDistance > distance) {
+				closestDistance = distance;
+				selectedPlayer = player;
+			}
+		}
+		return selectedPlayer;
+	}
 }
