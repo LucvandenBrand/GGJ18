b1bf3075e93ce359776668b7748fbc91720d0cbc
tincoboekestijn <tinco.boekestijn@gmail.com>
2018-01-27 12:11:56 +0100

    Random Cell AI

--- Assets/Scripts/DebrisController.cs
+++ Assets/Scripts/DebrisController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class DebrisController : MonoBehaviour {
+
+	public GameObject[] cells;
 
 	// Use this for initialization
 	void Start () {
@@ -13,4 +15,20 @@ public class NewBehaviourScript : MonoBehaviour {
 	void Update () {
 		
 	}
+
+//	GameObject findClosestCell () 
+//	{
+//		cells = GameObject.FindGameObjectsWithTag("Cells");
+//		GameObject selectedCell;
+//		float closestDistance = float.MaxValue;
+//		foreach(GameObject cell in cells ) {
+//			var pos = cell.transform.position;
+//			float distance = Mathf.Sqrt (Mathf.Pow(pos.x - transform.position.x, 2) + Mathf.Pow(pos.y - transform.position.y, 2));
+//			if (closestDistance > distance) {
+//				closestDistance = distance;
+//				selectedCell = cell;
+//			}
+//		}
+//		return closestDistance;
+//	}
 }
--- Assets/Scripts/coneGenerator.cs
+++ Assets/Scripts/coneGenerator.cs
@@ -2,17 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class coneGenerator : MonoBehaviour {
+public class ConeGenerator : MonoBehaviour {
     public GameObject cone;
     public Material mat;
+	private Rigidbody rb;
+
 	// Use this for initialization
 	void Start () {
-
         GameObject locCone = Instantiate(cone);
 		locCone.layer = LayerMask.NameToLayer ("ConesLayer");
         locCone.GetComponent<Renderer>().material = mat;
         locCone.transform.position = transform.position;
         locCone.transform.parent = transform;
 	}
-
 }
