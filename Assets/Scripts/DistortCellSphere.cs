249f34ec5a79d22c3f8973475431867fbbfdd20a
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-01-28 14:34:48 +0100

    Amazing Cell Graphic.

---
+++ Assets/Scripts/DistortCellSphere.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DistortCellSphere : MonoBehaviour {
+    MeshFilter meshfilter;
+    float phase;
+
+	// Use this for initialization
+	void Start () {
+      phase = Random.Range(0f, 24f);
+      meshfilter = GetComponent<MeshFilter>();
+
+      // Mesh mesh = GetComponent<MeshFilter>().mesh;
+      // Vector3[] vertices = mesh.vertices;
+      // Vector3[] normals = mesh.normals;
+      // int i = 0;
+      // while (i < vertices.Length) {
+      //     vertices[i] += 0.02f * normals[i] * Random.Range(-1f, 1f);
+      //     ++i;
+      // }
+      // mesh.vertices = vertices;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+      Mesh mesh = GetComponent<MeshFilter>().mesh;
+      Vector3[] vertices = mesh.vertices;
+      Vector3[] normals = mesh.normals;
+      int i = 0;
+      while (i < vertices.Length) {
+          vertices[i] += 0.004f * normals[i] * Mathf.Sin(4f * Time.time + 3 * i + phase);
+          ++i;
+      }
+      mesh.vertices = vertices;
+	}
+}
