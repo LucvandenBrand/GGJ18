cb4808fc97bd79a26977ce16ebfdc03baeebaf78
Qqwy/Wiebe-Marten Wijnja <w-m@wmcode.nl>
2018-02-20 10:28:13 +0100

    simple 2d movement example with acceleration

--- snappy_server/native/invectedgamelogic/src/game_logic.rs
+++ snappy_server/native/invectedgamelogic/src/game_logic.rs
@@ -7,9 +7,13 @@ use std::collections::hash_map::Entry;
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
 pub struct Player {
-    position: (f64, f64),
-    score: u64,
     name: String,
+    score: u64,
+
+    position: (f64, f64),
+    velocity: (f64, f64),
+    acceleration: (f64, f64),
+    desired_direction: (f64, f64),
 }
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -32,7 +36,14 @@ pub fn render_state(state: &InvectedGameState) -> String {
 /// Returns a new state with another player added.
 pub fn add_player(state: &InvectedGameState, player_name: String) -> InvectedGameState {
     let mut players = (*state.players).clone();
-    players.insert(player_name.clone(), Player {name: player_name, score: 0, position: (0.0, 0.0) });
+    players.insert(player_name.clone(), Player {
+        name: player_name,
+        score: 0,
+        position: (0.0, 0.0),
+        velocity: (0.0, 0.0),
+        acceleration: (0.0, 0.0),
+        desired_direction: (0.0, 0.0),
+    });
 
     InvectedGameState {game_round_time: state.game_round_time, players: Arc::new(players)}
 }
@@ -41,16 +52,60 @@ pub fn update_state(state: &InvectedGameState, movements: & Vec<(String, (f64, f
     println!("{:?}", state);
     println!("{:?}", movements);
     let mut players = (*state.players).clone();
-    for &(ref player_name, (xpos, ypos)) in movements {
+    for &(ref player_name, (xdir, ydir)) in movements {
         let name = player_name.clone();
         if let Entry::Occupied(mut player) = players.entry(name) {
             let score = player.get().score;
-            player.insert(Player {name: player_name.clone(), score: score, position: (xpos, ypos)});
+            let velocity = player.get().velocity;
+            let acceleration = player.get().acceleration;
+            let position = player.get().position;
+            player.insert(Player {
+                name: player_name.clone(),
+                score: score,
+
+                position: position,
+                velocity: velocity,
+                acceleration: acceleration,
+                desired_direction: (xdir, ydir)
+            });
         }
     }
     InvectedGameState {game_round_time: state.game_round_time, players: Arc::new(players)}
 }
 
+fn move_player(orig_player: &Player, dt: f64) -> Player {
+    let mut player = orig_player.clone();
+
+    let (mut acc_x, mut acc_y) = player.acceleration;
+    let (mut vel_x, mut vel_y) = player.velocity;
+    let (mut pos_x, mut pos_y) = player.position;
+    pos_x += vel_x * dt;
+    pos_y += vel_y * dt;
+    vel_x += acc_x * dt;
+    vel_y += acc_y * dt;
+    acc_x += player.desired_direction.0 * dt;
+    acc_y *= player.desired_direction.1 * dt;
+
+    // player.acceleration = (acc_x, acc_y);
+    // let new_player = Player {
+    //     name: *player_name,
+    //     score: player.score
+    // }
+    player.position = (pos_x, pos_y);
+    player.velocity = (vel_x, vel_y);
+    player.acceleration = (acc_x, acc_y);
+
+    player
+}
+
+fn move_players(state: &InvectedGameState, dt: f64) -> InvectedGameState {
+    let players: HashMap<String, Player> = (*state.players).values().map(|player| {
+        let player_name = player.name.clone();
+        (player_name, move_player(player, dt))
+    }).collect();
+
+    InvectedGameState {game_round_time: state.game_round_time, players: Arc::new(players)}
+}
 
 
 /// Prints information of a single player. Debugging function, will probably not be used later.
